cac9501ff45933fe64c6e6d1dcc8e6631b5b96b0
Yusuke Mukai <yusuke@mac.local>
2015-07-18 17:37:57 +0900

    implemented log erase feature

--- routes/index.js
+++ routes/index.js
@@ -6,24 +6,40 @@ var cfenv = require('cfenv');
 
 var conf = cfenv.getAppEnv().getServiceCreds('aws-mongo')
 
+var url = 'mongodb://' + conf.host + ':' + conf.port + '/' + conf.database;
+
 router.get('/', function(req, res) {
   res.render('index');
 });
 
 router.get('/:collection', function(req, res, next) {
   conf.collection = req.params.collection
-  var url = 'mongodb://' + conf.host + ':' + conf.port + '/' + conf.database;
   mongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
     console.log("Connected correctly to server");
     db.collection(conf.collection, function(err, collection) {
       collection.find({}).toArray(function(err, docs) {
-        console.log("insert success");
+        console.log("find success");
         db.close();
         res.render('collection', {
           conf: conf,
           docs: docs
         });
+      });
+    });
+  });
+});
+
+router.post('/:collection', function(req, res) {
+  conf.collection = req.params.collection
+  mongoClient.connect(url, function(err, db) {
+    assert.equal(null, err);
+    console.log("Connected correctly to server");
+    db.collection(conf.collection, function(err, collection) {
+      collection.remove({}, function(err, result) {
+        console.log("Removed the document with the field a equal to 3");
+        db.close();
+        res.redirect('/' + conf.collection);
       });
     });
   });
